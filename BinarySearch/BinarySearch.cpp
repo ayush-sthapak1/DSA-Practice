485c8832dc364d3a86941d4297ba9e1b7b9c0a7e
ayush-sthapak1 <ayushsthapak0@gmail.com>
2025-12-19 13:10:11 +0530

    19/12/25

---
+++ BinarySearch/BinarySearch.cpp
@@ -0,0 +1,25 @@
+// LeetCode 704: Binary Search
+// Approach: Classic binary search on sorted array
+// Time Complexity: O(log n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        return -1;
+    }
+};
---
+++ BinarySearch/FindFirstandLastPositionofElementinSortedArray.cpp
@@ -0,0 +1,46 @@
+// LeetCode 34: Find First and Last Position of Element in Sorted Array
+// Approach: Binary search (two passes)
+// Time Complexity: O(log n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    int firstPos(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] >= target) {
+                if (nums[mid] == target) ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    int lastPos(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] <= target) {
+                if (nums[mid] == target) ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        if (nums.empty()) return {-1, -1};
+        return { firstPos(nums, target), lastPos(nums, target) };
+    }
+};
